d32a1f24a67d641a246124c224e2f67cc64a8f4e
Joshua <joshuahsu108@gmail.com>
2024-04-09 17:43:40 -0700

    Better movement, including staying on ground when running down slopes

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -1,22 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -1300.0
+const SPEED = 1200.0
+const JUMP_VELOCITY = -1600.0
+
+var canParry = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 3
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		canParry = true
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("Jump")
-	if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and canParry:
 		$AnimatedSprite2D.play("Parry")
+		canParry = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -36,5 +41,8 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play("Block")
 	elif not direction and is_on_floor():
 		$AnimatedSprite2D.play("Idle")
+
+	floor_snap_length = 100
+	floor_constant_speed = true
 
 	move_and_slide()
