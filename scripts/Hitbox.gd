83195b7a4caa64e60d69206a972ba184c97605a2
BuildTools <unconfigured@null.spigotmc.org>
2024-04-28 22:55:46 -0700

    Update hitbox check logic

--- scripts/Hitbox.gd
+++ scripts/Hitbox.gd
@@ -10,9 +10,9 @@ func _init():
 	collision_mask = 2
 
 func _ready():
-	if hitbox == null:
-		return
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(hurtbox : Hurtbox):
+	if hurtbox == null:
+		return
 	triggered.emit(hurtbox)
