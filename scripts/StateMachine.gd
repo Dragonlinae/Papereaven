0bf3353051811699cf10a452821fe10dd97373ed
= <anphudung03@gmail.com>
2024-04-30 23:03:47 -0700

    fix vars to snake

--- scripts/StateMachine.gd
+++ scripts/StateMachine.gd
@@ -7,7 +7,7 @@ signal onStateTransition(previousState : String, currentState : String)
 signal onStateForceTransition(previousState : String, forcedState : String)
 
 # Declare member variables
-var currentState : String = "DEFAULT_START_STATE_PLACEHOLDER"
+var current_state : String = "DEFAULT_START_STATE_PLACEHOLDER"
 var valid_states : Array[String] = []
 
 var valid_transitions = {} # {[string] : Array[String]}
@@ -15,10 +15,10 @@ var valid_transitions = {} # {[string] : Array[String]}
 func validState(state : String) -> bool:
 	return valid_states.has(state)
 
-func validTransition(targetState : String) -> bool:
-	if not validState(targetState):
+func validTransition(target_state : String) -> bool:
+	if not validState(target_state):
 		return false
-	return valid_transitions[currentState].has(targetState)
+	return valid_transitions[current_state].has(target_state)
 
 func add_state(state : String):
 	if validState(state):
@@ -28,35 +28,35 @@ func add_state(state : String):
 	valid_transitions[state] = []
 	return
 
-func add_transition(originState : String, targetState : String):
-	if not validState(originState) or not validState(targetState):
+func add_transition(origin_state : String, target_state : String):
+	if not validState(origin_state) or not validState(target_state):
 		push_error("One or more parameter states is not a valid state in target state machine")
 		return
-	if valid_transitions[originState].has(targetState):
+	if valid_transitions[origin_state].has(target_state):
 		push_error("Duplicate transition added to target state machine")
 		return
-	valid_transitions[originState].append(targetState)
+	valid_transitions[origin_state].append(target_state)
 	return
 
-func override_state(forcedState : String):
-	var previousState : String = currentState
-	currentState = forcedState
-	onStateTransition.emit(previousState, currentState)
-	onStateForceTransition.emit(previousState, currentState)
+func override_state(forced_state : String):
+	var prev_state : String = current_state
+	current_state = forced_state
+	onStateTransition.emit(prev_state, current_state)
+	onStateForceTransition.emit(prev_state, current_state)
 
-func transition_state(targetState : String):
-	if not validTransition(targetState):
+func transition_state(target_state : String):
+	if not validTransition(target_state):
 		push_warning("Invalid state transition detected.")
 		return
-	if targetState == currentState:
+	if target_state == current_state:
 		push_warning("Self-looping state transition detected.")
 		return
-	var previousState : String = currentState
-	currentState = targetState
-	onStateTransition.emit(previousState, currentState)
+	var prev_state : String = current_state
+	current_state = target_state
+	onStateTransition.emit(prev_state, current_state)
 
 # Constructor
-func _init(defaultState : String):
-	currentState = defaultState
-	add_state(defaultState)
+func _init(default_state : String):
+	current_state = default_state
+	add_state(default_state)
 	return
