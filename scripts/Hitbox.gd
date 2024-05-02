060bab9c8fa3ed7825275a99adb1484a13f8f0cd
= <anphudung03@gmail.com>
2024-05-01 23:58:42 -0700

    Implement more of hitbox.gd

--- scripts/Hitbox.gd
+++ scripts/Hitbox.gd
@@ -1,16 +1,26 @@
 class_name Hitbox
 extends Area2D
 
+@export var hitbox_state : HitboxState = HitboxState.new()
+
 # Declare Signals
 signal triggered(hurtbox : Hurtbox)
 
 # Constructor & Methods
 func _init():
 	collision_layer = 0
-	collision_mask = 2
+	collision_mask = 0
 
 func _ready():
 	area_entered.connect(_on_area_entered)
+
+func enable_hitbox():
+	collision_mask = 2
+	return
+
+func disable_hitbox():
+	collision_mask = 0
+	return
 
 func _on_area_entered(hurtbox : Hurtbox):
 	if hurtbox == null:
