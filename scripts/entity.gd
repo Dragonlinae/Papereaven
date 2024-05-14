3dc2302a8af9bc76bfa814da80eb712dc8995324
AnPhu-D <anphudung03@gmail.com>
2024-05-14 13:47:56 -0700

    Add damage reduction

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -10,11 +10,13 @@ class_name Entity
 ## Should the tree be removed when health <= 0
 @export var destroy_when_dead: bool = true
 
+var damage_factor: float = 1.0
+
 func restore_health():
 	current_health = max_health
 
 func take_damage(damage: int):
-	current_health -= damage
+	current_health -= int(damage * damage_factor)
 	if is_dead() and destroy_when_dead:
 		queue_free()
 
@@ -23,3 +25,7 @@ func is_dead():
 
 func is_alive():
 	return current_health > 0
+
+func set_damage_factor(new_damage_factor: float):
+	print("Set new damage factor to", new_damage_factor)
+	damage_factor = new_damage_factor
