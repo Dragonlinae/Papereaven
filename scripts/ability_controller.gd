0548d042de55ef3c03d02d1386160031730e5106
AnPhu-D <anphudung03@gmail.com>
2024-05-30 13:47:34 -0700

    Implement unlock lock to ability cast

--- scripts/ability_controller.gd
+++ scripts/ability_controller.gd
@@ -5,11 +5,12 @@ extends Node
 @export var entity: Entity
 @export var input_handler: PlayerInputHandler
 
+@export var ability1_unlocked: bool
 @export var ability1: Ability
 
 func cast_ability1():
 	ability1.cast_ability()
 
 func _process(_delta):
-	if input_handler.get_ability_input():
+	if input_handler.get_ability_input() and ability1_unlocked:
 		cast_ability1()
