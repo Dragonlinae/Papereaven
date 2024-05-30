57b053de53e85748d62f27dcc3bd2c021b75e871
Kwok-Ho <khwithcoding@gmail.com>
2024-05-30 05:54:13 -0700

    Fixed Door Gravity bug. Also included Door Placeholder Texture when opened instead of falling off on the map

--- scripts/DoorButton2.gd
+++ scripts/DoorButton2.gd
@@ -3,6 +3,7 @@ class_name DoorButton2
 
 var activated = false
 var activated_texture = load("res://assets/ButtonPlaceholder2.png");
+var activated_door = load("res://assets/DoorPlaceholderOpened.png");
 
 @export var door_rigidbody: RigidBody2D
 
@@ -11,6 +12,6 @@ func _on_area_2d_area_entered(area):
 	if area.get_parent() is CharacterController && !activated:
 		activated = true
 		$ButtonSprite.texture = activated_texture
-		print("Button")
+		$RigidBody2D/DoorSprite.texture = activated_door
 		door_rigidbody.collision_layer = 0
 		door_rigidbody.collision_mask = 0
