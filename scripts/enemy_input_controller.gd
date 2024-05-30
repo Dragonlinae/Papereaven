070de253c82e7edacbc661e2b54a9d486dd9535d
AnPhu-D <anphudung03@gmail.com>
2024-05-30 11:34:57 -0700

    Implement player following to enemy and distance keeping

--- scripts/enemy_input_controller.gd
+++ scripts/enemy_input_controller.gd
@@ -9,7 +9,40 @@ func _init():
 # TODO: Implement enemy logic state machine
 
 func get_movement_direction() -> float:
-	return 0.5
+	var root_node: Node = get_tree().get_root() as Node
+	var scene_root: Node2D = root_node.get_node("Node2D") as Node2D
+	var player: CharacterController = scene_root.get_node("Player") as CharacterController
+	var enemy: CharacterController = get_parent() as CharacterController
+
+	if enemy == null:
+		return 0
+
+	var player_collision_shape: CollisionShape2D = player.get_node("CharacterCollisionShape") as CollisionShape2D
+	var enemy_collision_shape: CollisionShape2D = enemy.get_node("CharacterCollisionShape") as CollisionShape2D
+
+	var player_position: Vector2 = player_collision_shape.global_position as Vector2
+	var enemy_position: Vector2 = enemy_collision_shape.global_position as Vector2
+
+	var player_x: float = player_position.x
+	var enemy_x: float = enemy_position.x
+
+	var distance_to_player: float = abs(player_x - enemy_x)
+
+	var signFlip = 1
+	print(distance_to_player)
+	if distance_to_player < 200:
+		signFlip = -1
+	if distance_to_player < 350 and distance_to_player > 250:
+		print(distance_to_player < 300)
+		print(distance_to_player > 200)
+		# return 0
+
+	if (player_x > enemy_x):
+		print("Moving Right")
+		return 1 * signFlip
+	else:
+		print("Moving Left")
+		return - 1 * signFlip
 
 func get_jump_input() -> bool:
 	return false
