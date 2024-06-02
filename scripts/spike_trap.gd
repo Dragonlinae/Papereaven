0bfb4eeec40f774458d550b22bee94114fcf62a7
SafeDuck <schopra579@gmail.com>
2024-06-02 15:06:43 -0700

    Spike Trap Puzzle

---
+++ scripts/spike_trap.gd
@@ -0,0 +1,10 @@
+class_name SpikeTrap extends Hitbox
+
+@export var actuate:bool = true
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super()
+	if actuate:
+		$AnimationPlayer.play("activate")
+	else:
+		$CollisionShape2D.disabled = false
